db5bc40298dc66acf3b3159749e1755335430a40
ritik rana <Ritik>
2022-11-30 11:48:35 +0530

    login page

---
+++ login.js
@@ -0,0 +1,14 @@
+let regUser="ritik@gmail.com";
+let regPass="ritik@123";
+ let entUser="ritik@gmail.com";
+ entPass="ritik@123";
+
+ if(regUser==entUser){
+if(regPass==entPass){
+    console.log("Login Successfully")
+}else{
+    console.log("Wrong Pass");
+}
+ }else{
+    console.log("Invalid Email")
+ }
